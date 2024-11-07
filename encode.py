0d9f0e55d8142a2547a43908d7949e5e03ec3d60
Harrison Baghurst <harrison070806@gmail.com>
2024-11-07 20:55:52 +0000

    added text file reading and conversion to binary format

--- encode.py
+++ encode.py
@@ -2,18 +2,34 @@ import png
 
 def encode(msg_path="inputs\\input_message.txt", img_path="inputs\\input_image.png"): 
 
-    # read image 
+    ### ---------- read text file ---------- ###
+    text_file = open(msg_path, 'r') 
+    text = text_file.readline() 
+    msg_binary = '' 
+    for char in text: 
+
+        # convert char to binary code in exact 8 bit format
+        binary = bin(ord(char)).replace('0b', '') 
+        while len(binary) < 8: 
+            binary = '0' + binary
+
+        # add char binary to msg_binary 
+        msg_binary += binary         
+
+    ### ---------- read image ---------- ###
     read_image = png.Reader(filename=img_path).asDirect()
     pixels = [list(row) for row in read_image[2]]
 
+    # if input image has alpha layer remove alpha layer 
     if read_image[3]['alpha']: 
         for i in range(len(pixels)): 
             del pixels[i][3::4]
 
+    # get width and height of input image 
     width = read_image[3]['size'][0] 
     height = read_image[3]['size'][1] 
 
-    # output image 
+    ### ---------- output image ---------- ###
     write_image = png.Writer(width, height, greyscale=False) 
     write_image.write(open("outputs\\output_image.png", "wb"), pixels) 
 
